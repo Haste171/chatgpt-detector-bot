d8f132ee2a22acf92b6b755cc2923a9faeade3df
Haste171 <34923485+Haste171@users.noreply.github.com>
2023-03-07 11:26:00 -0600

    Delete demo.py

--- demo.py
+++
@@ -1,8 +0,0 @@
-from gptzero import GPTZeroAPI
-
-api_key = 'b2102ef5c89d46c8bfc2b16b99cf2c87' # Your API Key from https://gptzero.me
-gptzero_api = GPTZeroAPI(api_key)
-
-document = 'Hello world!'
-response = gptzero_api.text_predict(document)
-print(response)
